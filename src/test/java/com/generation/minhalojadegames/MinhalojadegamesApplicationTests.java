c2495a9547368c5a034fa237ee11c3e559dae592
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-02-23 16:11:02 -0300

    criando minha loja de games

---
+++ src/test/java/com/generation/minhalojadegames/MinhalojadegamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.minhalojadegames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhalojadegamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
